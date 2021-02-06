ef2faa03a72c2beb39c1c06766b431d1e0a66c16
daniel <daniel@codein.ca>
2021-02-06 17:08:24 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+// const GITHUB = process.env.REACT_APP_GITHUB_TOKEN;
+const GITHUB_URL = "https://api.github.com/users/w-b-dev/starred";
+const Cards = ({ cards }: { cards: {}[] }) => {
+  const _cards = cards.map((card: any) => {
+    const { id, html_url, name, owner } = card;
+    return (
+      <section key={id}>
+        <a href={html_url}>{owner.login}</a>
+        <p>{name}</p>
+        <img
+          src={owner.avatar_url}
+          alt={owner.login}
+          height={"100px"}
+          width={"100px"}
+        />
+        {/*<pre>{JSON.stringify(card, null, 2)}</pre>*/}
+      </section>
+    );
+  });
+  return <>{_cards}</>;
+};
 
 function App() {
+  const [cards, setCards] = useState([]);
+  useEffect(() => {
+    const ignoreResponse = { status: false };
+    const fetchStars = async () => {
+      const payload = await fetch(GITHUB_URL);
+      const json = await payload.json();
+      if (!ignoreResponse.status) setCards(json);
+    };
+    fetchStars();
+    return () => {
+      ignoreResponse.status = true;
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <article>
+        <Cards cards={cards} />
+      </article>
+    </main>
   );
 }
 
